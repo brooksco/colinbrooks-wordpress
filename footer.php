8507040196694eaebe6beb61e44b274b01e00041
Colin Brooks <colinbooks@gmail.com>
2018-08-04 13:32:13 -0700

    rawr

--- footer.php
+++ footer.php
@@ -18,6 +18,7 @@
 		<li><a href="https://www.flickr.com/pandadumpster" rel="noopener">Flickr</a></li>
 		<li><a href="https://github.com/brooksco" rel="noopener">GitHub</a></li>
 		<li><a href="https://instagram.com/colinbooks" rel="noopener">Instagram</a></li>
+        <li><a href="https://medium.com/@colinbooks" rel="noopener">Medium</a></li>
 		<li><a href="https://www.linkedin.com/in/brooksco" rel="noopener">LinkedIn</a></li>
 		<li><a href="http://colinbrooks.photography/" rel="noopener">Tumblr</a></li>
 		<li><a href="https://www.twitter.com/colinbooks" rel="noopener">Twitter</a></li>
