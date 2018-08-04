6f18be83edbb18612c35e24c2ea425057d4808e4
Colin Brooks <colinbooks@gmail.com>
2018-08-04 13:20:57 -0700

    rawr

--- functions.php
+++ functions.php
@@ -7,79 +7,79 @@
  * @package colinbooks
  */
 
-if ( ! function_exists( 'colinbooks_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function colinbooks_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on colinbooks, use a find and replace
-		 * to change 'colinbooks' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'colinbooks', get_template_directory() . '/languages' );
+if (!function_exists('colinbooks_setup')) :
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function colinbooks_setup() {
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on colinbooks, use a find and replace
+         * to change 'colinbooks' to the name of your theme in all the template files.
+         */
+        load_theme_textdomain('colinbooks', get_template_directory() . '/languages');
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support('automatic-feed-links');
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support('title-tag');
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support('post-thumbnails');
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'colinbooks' ),
-		) );
+        // This theme uses wp_nav_menu() in one location.
+        register_nav_menus(array(
+            'menu-1' => esc_html__('Primary', 'colinbooks'),
+        ));
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'gallery',
-			'caption',
-		) );
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support('html5', array(
+            'search-form',
+            'gallery',
+            'caption',
+        ));
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'colinbooks_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
+        // Set up the WordPress core custom background feature.
+        add_theme_support('custom-background', apply_filters('colinbooks_custom_background_args', array(
+            'default-color' => 'ffffff',
+            'default-image' => '',
+        )));
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+        // Add theme support for selective refresh for widgets.
+        add_theme_support('customize-selective-refresh-widgets');
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
+        /**
+         * Add support for core custom logo.
+         *
+         * @link https://codex.wordpress.org/Theme_Logo
+         */
+        add_theme_support('custom-logo', array(
+            'height' => 250,
+            'width' => 250,
+            'flex-width' => true,
+            'flex-height' => true,
+        ));
+    }
 endif;
-add_action( 'after_setup_theme', 'colinbooks_setup' );
+add_action('after_setup_theme', 'colinbooks_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -89,12 +89,13 @@ add_action( 'after_setup_theme', 'colinbooks_setup' );
  * @global int $content_width
  */
 function colinbooks_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'colinbooks_content_width', 640 );
+    // This variable is intended to be overruled from themes.
+    // Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+    // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+    $GLOBALS['content_width'] = apply_filters('colinbooks_content_width', 640);
 }
-add_action( 'after_setup_theme', 'colinbooks_content_width', 0 );
+
+add_action('after_setup_theme', 'colinbooks_content_width', 0);
 
 /**
  * Register widget area.
@@ -102,30 +103,32 @@ add_action( 'after_setup_theme', 'colinbooks_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function colinbooks_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'colinbooks' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'colinbooks' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+    register_sidebar(array(
+        'name' => esc_html__('Sidebar', 'colinbooks'),
+        'id' => 'sidebar-1',
+        'description' => esc_html__('Add widgets here.', 'colinbooks'),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ));
 }
-add_action( 'widgets_init', 'colinbooks_widgets_init' );
+
+add_action('widgets_init', 'colinbooks_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
 function colinbooks_scripts() {
-	wp_enqueue_style( 'colinbooks-style', get_stylesheet_uri() );
+    wp_enqueue_style('colinbooks-style', get_stylesheet_uri());
 
-	wp_deregister_script('jquery'); 
-	wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.3.1.min.js', array(), null, true);
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.3.1.min.js', array(), null, true);
 
-	wp_enqueue_script( 'colinbooks-app', get_template_directory_uri() . '/js/app.min.js', array(), '20151215', true );
+    wp_enqueue_script('colinbooks-app', get_template_directory_uri() . '/js/app.min.js', array(), '20151215', true);
 }
-add_action( 'wp_enqueue_scripts', 'colinbooks_scripts' );
+
+add_action('wp_enqueue_scripts', 'colinbooks_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -150,11 +153,24 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+if (defined('JETPACK__VERSION')) {
+    require get_template_directory() . '/inc/jetpack.php';
 }
 
 function add_rel_to_gallery($link, $id) {
-  return str_replace('<a href=', '<a data-fancybox="gallery" href=', $link);
+    return str_replace('<a href=', '<a data-fancybox="gallery" href=', $link);
 }
-add_filter( 'wp_get_attachment_link', 'add_rel_to_gallery', 10, 2 );
+
+add_filter('wp_get_attachment_link', 'add_rel_to_gallery', 10, 2);
+
+function prefix_conditional_body_class($classes) {
+    if (is_page_template('gallery.php')) {
+        $classes[] = 'template-photography';
+    } else if (is_page_template('home.php')) {
+        $classes[] = 'template-home';
+    }
+
+    return $classes;
+}
+
+add_filter('body_class', 'prefix_conditional_body_class');
