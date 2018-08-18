60215bd8cfa83fc6770ac12d85552868320d8494
Colin Brooks <colinbooks@gmail.com>
2018-08-18 12:46:46 -0400

    tweaks

--- home.php
+++ home.php
@@ -29,7 +29,7 @@ get_header();
 
         <?php endwhile; // End of the loop. ?>
 
-        <h2 class="posts__title">Recent work</h2>
+        <h2 class="posts__title">Projects</h2>
 
         <ul class="posts">
             <?php
