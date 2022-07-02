10c576e2cfe8cbcd528b03a5e7473d004ed622ed
brooksco <colinbooks@gmail.com>
2022-07-02 00:28:45 -0400

    tweaked styling dropped tumblr

--- footer.php
+++ footer.php
@@ -18,9 +18,8 @@
 		<li><a href="https://www.flickr.com/pandadumpster" rel="noopener">Flickr</a></li>
 		<li><a href="https://github.com/brooksco" rel="noopener">GitHub</a></li>
 		<li><a href="https://instagram.com/colinbooks" rel="noopener">Instagram</a></li>
-		<li><a href="https://medium.com/@colinbooks" rel="noopener">Medium</a></li>
 		<li><a href="https://www.linkedin.com/in/brooksco" rel="noopener">LinkedIn</a></li>
-		<li><a href="http://colinbrooks.photography/" rel="noopener">Tumblr</a></li>
+		<li><a href="https://medium.com/@colinbooks" rel="noopener">Medium</a></li>
 		<li><a href="https://www.twitter.com/colinbooks" rel="noopener">Twitter</a></li>
 	</ul>
 
